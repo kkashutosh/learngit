9a457a033e09f37847a351c173630572e1d7afd7
ashutosh_123 <ashutosh.kumar@talenq.com>
2019-06-15 15:40:54 +0530

    Added index.html

---
+++ mainpage.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="image.css">
+   
+</head>
+
+
+<body style= "background-color:#f2f2f2;">
+
+
+  
+<div class="container" style="width:100%;background-color: ">
+  <div class="row" >
+    <div class="col-lg-3" style="background-color: ;padding-left: 50px" >
+        <h1 style="color:#0d1251 "> Notice Board</h1>
+      
+    </div>
+
+    <div class="col-lg-6" style="background-color: ;" >
+
+       <img src="c2.png" width="100%" height="100%">
+      
+    </div>
+
+    <div class="col-lg-3" style="padding-left: 200px; background-color: ;">
+     <a href="admin/admin.php"><button type="submit" class="btn btn-primary" style=" background-color: #767bc1; padding-top: auto;padding-left: auto;padding-right: auto;padding-bottom: auto; margin-top:10px; font-size: 20px;" >Admin</button></a>
+      
+    </div>
+    
+  </div>
+</div>
+
+ <hr style="color: ; border-top: 3px solid black;width: 90%;margin-top: 30px">
+
+  <div class="container" align="center" style="background-color: ;">
+
+ <img src="mlogo.png" width="160px" height="200px">
+    
+  </div>
+
+   <div class="container" align="center">
+
+ <h2 style="color:  ">Log In</h2>
+    
+  </div>
+
+  <hr style="color: red; border-top: 3px solid black;width: 90%">
+
+  
+
+    <div class="container" align="center" style="margin-top: 50px;  ">
+      <div class="row" >
+       <div class="col-lg-5" style="background-color: ;padding-left: 50px;" >
+           <a href="student/student.php"><button type="submit" class="btn btn-primary" style=" background-color:  #f91d1d; padding-top: auto;padding-left: 200px;padding-right: 200px;padding-bottom: auto; margin-top:10px; font-size: 20px;" >Student</button></a>
+      
+      
+       </div>
+       <div class="col-lg-2" style="background-color: ;" >
+        <h3 style="padding-left: 25px;"> or </h3>
+      </div>
+
+       <div class="col-lg-5" style="background-color: ;" >
+       
+      <a href="faculty/faculty.php"><button type="submit" class="btn btn-primary" style="background-color:#38ce35; padding-top: auto;padding-left: 200px;padding-right: 200px;padding-bottom: auto; margin-top:10px; font-size: 20px;" >faculty</button></a>
+       
+      </div>
+     </div>
+    </div>  
+
+</body>
+</html>
